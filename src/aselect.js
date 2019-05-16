8a325b0575a12bf161eba59e3a608dda13875808
iDenisM <denis.mitiliuc@gmail.com>
2019-05-17 01:01:09 +0200

    git commit 2

--- src/aselect.js
+++ src/aselect.js
@@ -1,19 +1,30 @@
-import './aselect.listbox';
-import './aselect.listboxButton';
+import './ASelectListbox';
+import './ASelectListboxButton';
 
 (function () {
 	var api = {
-		version: '1.0.0',
+    version: '1.0.0',
+    
+    create: function(selector) {
+
+    },
 
 		// Used to reset a select if it was already set
 		// Used to create a custom select if no one was created yet
 		reset: function(selector) {
-			// Check if select was created and recreate it with new options
+      
+      for (let elem of document.querySelectorAll(selector)) {
+        // Check if select was created and recreate it with new options
+        if (selector.dataset.aselect) {
 
-			// If no select was created create new
-			for (let elem of document.querySelectorAll(selector)) {
+        }
+
+        // If no select was created create new
         elem.classList.add('hidden');
-        
+
+        // Create new lisboxButton
+        // Create new lisbox
+        // Append all to lisbox wrapper
 			}
 		}
 	}
